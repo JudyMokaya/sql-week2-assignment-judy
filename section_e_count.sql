24da7d44f1dc6c59f0c32b08ec02e0e52fa54a81
Tildah <judymokayakwamboka@gmail.com>
2026-07-17 17:03:48 +0300

    Add SQL queries for student count and exam results

--- section_e_count.sql
+++ section_e_count.sql
@@ -1,1 +1,15 @@
+-- Section E: COUNT
+-- Name : Judy Mokaya
+-- Date: 17-07-2026
+-- =====================================================
+
+-- Q27. How many students are currently in Form 3? Write the query.
+select count(*) as form3_students
+from greenwood_academy.students
+where class = 'Form 3';
+
+-- Q28. How many exam results have a mark of 70 or above? Write the query.
+select count(*) as num_pass_marks
+from greenwood_academy.exam_results
+where marks >= 70;
 
